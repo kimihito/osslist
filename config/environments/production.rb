ee890fb7bd405e1d66127a16055a3e16ab2c3bac
kimihito <tatsurotamashiro@gmail.com>
2020-08-08 06:44:01 +0900

    Rename app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fullstaq_ruby_on_heroku_production"
+  # config.active_job.queue_name_prefix = "osslist_production"
 
   config.action_mailer.perform_caching = false
 
